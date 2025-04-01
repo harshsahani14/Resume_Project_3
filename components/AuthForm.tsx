b28f95a0b6b38e1449d235162aba2bec6da792e3
Harsh10 <harshsahani078@gmail.com>
2025-04-01 22:37:45 +0530

    First commit

---
+++ components/AuthForm.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Image from "next/image"
+
+
+import { Button } from "@/components/ui/button"
+import FormField from "./FormField"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+import { toast } from "sonner"
+import { Form } from "./ui/form"
+
+
+const AuthFormSchema = (  type:FormType ) =>{
+
+    return z.object({
+        name: type==="sign-in" ? z.string().min(0)  : z.string().min(3),
+        email : z.string().email(),
+        password : z.string().min(3)
+    })
+
+}
+
+
+
+const AuthForm = ({type}:{type:FormType}) => {
+
+    const formSchema = AuthFormSchema(type);
+// 1. Define your form.
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name:"",
+      email:"",
+      password:""
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    
+    try{
+        if(type==='sign-in'){
+            toast.success("Less go")
+        }
+        else{
+            console.log('sign up')
+        }
+    }
+    catch(error){
+        console.log(error);
+        toast.error(`Something went wrong ${error} `)
+    }
+    console.log(values)
+  }
+
+  const isSignIn = type === "sign-in";
+  return (
+
+
+    <div className=" card-border lg:min-w-[566px]">
+
+        <div className=" flex flex-col gap-6 card py-14 px-10">
+
+            <div className=" flex gap-2 justify-center">
+
+                <Image src="/logo.svg" alt="logo"
+                    width={32}
+                    height={38}
+                />
+                <h2 className="text-primary-100">PrepWise</h2>
+            </div>
+        
+
+            <h3 className="text-center text-[28px] leading-[32px]">Practice job Interview with AI</h3>
+    
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className=" w-full space-y-6 mt-4 form">
+        
+        {!isSignIn && 
+          (<FormField
+            name='name'
+            placeholder='Your Name'
+            label='Name'
+            control={form.control}
+            
+            ></FormField>)
+        
+        }
+        
+        <FormField
+            name='email'
+            placeholder='Your email address'
+            label='Email Address'
+            control={form.control}
+            type="email"
+          ></FormField>
+        
+        <FormField
+            name='password'
+            placeholder='Enter your Password'
+            label='Password'
+            control={form.control}
+            type="password"
+            
+          ></FormField>
+
+        <Button className=" btn" type="submit">{ isSignIn ? "Sign in" : "Create account" }</Button>
+      </form>
+    </Form>
+
+    <p className=" text-center">
+
+        {isSignIn ? "No account yet?" : "Have an account already?" }
+
+        <Link href={isSignIn ? "/sign-up" : "/sign-in"} className=" font-bold text-user-primary ml-1"
+        > {isSignIn ? "Sign up" : "Sign in" } </Link>
+    </p>
+    </div>
+    </div>
+  )
+}
+
+export default AuthForm
---
+++ components/FormField.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+import { FormItem, FormLabel, FormControl, FormDescription, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+
+interface FormFieldProps<T extends FieldValues>{
+
+    control:Control<T>
+    name:Path<T>
+    label: string
+    placeholder?:string
+    type?: 'text' | 'email' | 'password' | 'file'
+}
+
+const FormField = ({ name ,label,placeholder,control,type="text" } : FormFieldProps<T>) => (
+
+
+    <Controller
+        name={name}
+        control={control}
+
+        render={ ({field})=>(
+
+            <FormItem>
+              <FormLabel className='  text-light-100'>{label}</FormLabel>
+              <FormControl>
+                <Input className="input" placeholder={placeholder} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )
+    
+        }
+
+    />
+    
+    
+    
+)
+
+export default FormField
